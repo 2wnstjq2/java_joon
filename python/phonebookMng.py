128e942e32f607784f11b6e49271d5b498bb1283
박준섭 <2wnstjq2@naver.com>
2020-02-11 15:00:45 +0900

    python
    야구게임 만들기 test
    나머지 폰북

---
+++ python/phonebookMng.py
@@ -0,0 +1,49 @@
+# 데이터 저장을 위한 클래스
+# 기능 클래스 -> 기능 모듈
+# 데이터를 저장하고 있는 배열(리스트) -> []
+# 기능 메서드 : 입력(리스트), 삭제(리스트 삭제), 검색, 전체출력
+
+# 친구의 정보 저장할 리스트
+pBooks = []
+
+# cnt = 0
+
+def insertmember():
+    # global cnt
+    name = input('이름을 입력해 주세요')
+    pNum = input('전화번호를 입력해 주세요')
+    bDay = input('생일을 입력해 주세요')
+    
+    member = {       
+        'name' : name,
+        'phonenumber' : pNum,
+        'birthday' : bDay        
+    }
+    pBooks.append(member)
+    # cnt+=1
+
+
+def showlist():
+    for member in pBooks:
+        print(member)
+        
+
+def deletemember():
+    x = input('삭제할 대상의 이름을 적어주세요')
+    for member in pBooks:
+        if x==member['name']:
+            del pBooks[pBooks.index(member)]
+            break
+        else:
+            print('찾으시는 대상이 없습니다')
+
+def editmember():
+    x = input('수정할 대상의 이름을 적어주세요')
+    for member in pBooks:
+        if x==member['name']:
+            del pBooks[pBooks.index(member)]
+            insertmember()
+            break
+        else:
+            print('찾으시는 대상이 없습니다')
+    
